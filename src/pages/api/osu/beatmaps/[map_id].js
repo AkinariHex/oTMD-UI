6f531ef6713e3c7db00a684bdc2a3e49fb2a49ea
Francesco <frawolf@outlook.it>
2021-11-08 12:29:46 +0100

    feat: Made proxy endpoint for osu beatmaps

---
+++ src/pages/api/osu/beatmaps/[map_id].js
@@ -0,0 +1,10 @@
+export default async function handler(req, res) {
+  const { map_id } = req.query;
+  const { Authorization } = req.headers;
+
+  const data = await fetch(`https://osu.ppy.sh/api/v2/beatmaps/${map_id}`, {
+    headers: { Authorization },
+  }).then((res) => res.json());
+
+  return res.json(data);
+}
