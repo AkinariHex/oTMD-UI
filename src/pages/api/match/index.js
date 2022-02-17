e6ecfa273dd557ee296393dde1630f424ea68a68
AkinariHex <akinari@outlook.it>
2022-02-17 16:33:58 +0100

    Update index.js

--- src/pages/api/match/index.js
+++ src/pages/api/match/index.js
@@ -32,90 +32,86 @@ export default async function handler(req, res) {
         view: "Grid view",
         pageSize: 1,
       })
-      .eachPage(
-        function page(records, fetchNextPage) {
-          if (records.length !== 0) {
-            if (body.stage == "Qualifiers") {
-              base("Matches").create(
-                [
-                  {
-                    fields: {
-                      UUID: uuidv4(),
-                      Owner: body.me,
-                      MatchID: body.matchID,
-                      MatchType: body.matchType,
-                      Stage: body.stage,
-                      Tournament: JSON.stringify(body?.tournament),
-                      Player: JSON.stringify(body?.player),
-                      Scores: JSON.stringify(body?.scores),
-                      TotalMaps: body.totalMaps,
-                      StartTime: body.matchStart,
-                    },
-                  },
-                ],
-                function (err, records) {
-                  if (err) {
-                    console.error(err);
-                    return res.status(404).json({ error: err });
-                  }
-                }
-              );
-            } else {
-              base("Matches").create(
-                [
-                  {
-                    fields: {
-                      UUID: uuidv4(),
-                      Owner: body.me,
-                      MatchID: body.matchID,
-                      MatchType: body.matchType,
-                      Stage: body.stage,
-                      BestOF: body.bestOF,
-                      Warmups: body.warmups,
-                      Tournament: JSON.stringify(body?.tournament),
-                      Players: JSON.stringify(body?.players),
-                      Teams: JSON.stringify(body?.teams),
-                      Scores: JSON.stringify(body?.scores),
-                      StartTime: body.matchStart,
-                    },
-                  },
-                ],
-                function (err, records) {
-                  if (err) {
-                    console.error(err);
-                    return res.status(404).json({ error: err });
-                  }
-                }
-              );
-            }
-
-            var { SendMatchesDiscord, DiscordChannelsMatch } =
-              records[0].fields;
-            DiscordChannelsMatch = JSON.parse(DiscordChannelsMatch);
-
-            if (
-              SendMatchesDiscord === "true" &&
-              DiscordChannelsMatch.length > 0
-            ) {
-              DiscordChannelsMatch.forEach(async (channel) => {
-                await match(body, channel);
-              });
-              return res.status(200).json({ status: "done" });
-            } else {
-              return res
-                .status(404)
-                .send({ error: "You haven't enabled the Discord Webhooks" });
-            }
-          } else {
-            return res.status(404).json({ message: "Wrong apikey!" });
-          }
-        },
-        function done(err) {
-          if (err) {
-            console.error(err);
-          }
+      .firstPage(function page(err, records) {
+        if (err) {
+          console.error(err);
+          return;
         }
-      );
+        if (records.length !== 0) {
+          if (body.stage == "Qualifiers") {
+            base("Matches").create(
+              [
+                {
+                  fields: {
+                    UUID: uuidv4(),
+                    Owner: body.me,
+                    MatchID: body.matchID,
+                    MatchType: body.matchType,
+                    Stage: body.stage,
+                    Tournament: JSON.stringify(body?.tournament),
+                    Player: JSON.stringify(body?.player),
+                    Scores: JSON.stringify(body?.scores),
+                    TotalMaps: body.totalMaps,
+                    StartTime: body.matchStart,
+                  },
+                },
+              ],
+              function (err, records) {
+                if (err) {
+                  console.error(err);
+                  return res.status(404).json({ error: err });
+                }
+              }
+            );
+          } else {
+            base("Matches").create(
+              [
+                {
+                  fields: {
+                    UUID: uuidv4(),
+                    Owner: body.me,
+                    MatchID: body.matchID,
+                    MatchType: body.matchType,
+                    Stage: body.stage,
+                    BestOF: body.bestOF,
+                    Warmups: body.warmups,
+                    Tournament: JSON.stringify(body?.tournament),
+                    Players: JSON.stringify(body?.players),
+                    Teams: JSON.stringify(body?.teams),
+                    Scores: JSON.stringify(body?.scores),
+                    StartTime: body.matchStart,
+                  },
+                },
+              ],
+              function (err, records) {
+                if (err) {
+                  console.error(err);
+                  return res.status(404).json({ error: err });
+                }
+              }
+            );
+          }
+
+          var { SendMatchesDiscord, DiscordChannelsMatch } = records[0].fields;
+          DiscordChannelsMatch = JSON.parse(DiscordChannelsMatch);
+
+          if (
+            SendMatchesDiscord === "true" &&
+            DiscordChannelsMatch.length > 0
+          ) {
+            DiscordChannelsMatch.forEach(async (channel) => {
+              await match(body, channel);
+            });
+            return res.status(200).json({ status: "done" });
+          } else {
+            return res
+              .status(404)
+              .send({ error: "You haven't enabled the Discord Webhooks" });
+          }
+        } else {
+          return res.status(404).json({ message: "Wrong apikey!" });
+        }
+      });
   }
 }
 
@@ -148,8 +144,11 @@ export default async function handler(req, res) {
 }; */
 
 const match = async (body, channel) => {
+  var results = "";
+  var Data = "";
+
   if (body.stage !== "Qualifiers") {
-    var results =
+    results =
       body.scores.winner === 1
         ? `:first_place: :red_circle: **${
             body.matchType === "1vs1"
@@ -170,7 +169,7 @@ const match = async (body, channel) => {
               : `${body.scores.team2} | ${body.teams[1]}`
           }** :blue_circle: :first_place:`;
 
-    var Data = {
+    Data = {
       embeds: [
         {
           author: {
@@ -187,6 +186,45 @@ const match = async (body, channel) => {
         })`, */
           url: `https://osu.ppy.sh/community/matches/${body.matchID}`,
           description: `${results}\n\n${body.matchType} - ${body.stage} - BO${body.bestOF} - ${body.warmups} warmups - [MP Link](https://osu.ppy.sh/community/matches/${body.matchID})`,
+          color: 0x4876b6,
+        },
+      ],
+    };
+  }
+
+  if (body.stage === "Qualifiers") {
+    var mods = {
+      NM: "<:nomod:868095234217750558>",
+      NF: "<:nofail:868095234230353960>",
+      EZ: "<:easy:868095234108710922>",
+      HD: "<:hidden:868095234150658079>",
+      HR: "<:hardrock:868095234129690664>",
+      DT: "<:doubletime:868095234079350844>",
+      HT: "<:halftime:943890234330972160>",
+      FL: "<:flashlight:868095233932533781>",
+    };
+
+    await body.scores.list.forEach((object) => {
+      let modstring = "";
+      object.mods.forEach((mod) => {
+        modstring += mods[mod];
+      });
+      results += `${modstring} ${object.score}\n`;
+    });
+
+    Data = {
+      embeds: [
+        {
+          author: {
+            name: `${body.tournament.acronym} ${
+              body.tournament.name !== "" ? `- ${body.tournament.name}` : ""
+            }`,
+            url: `https://osu.ppy.sh/community/matches/${body.matchID}`,
+            icon_url: `https://akinariosu.s-ul.eu/f72xTlsv`,
+          },
+          title: `${body.player.username}'s Qualifier`,
+          url: `https://osu.ppy.sh/community/matches/${body.matchID}`,
+          description: `${results}**Average: ${body.scores.average}** - ${body.totalMaps} maps - [MP Link](https://osu.ppy.sh/community/matches/${body.matchID})`,
           color: 0x4876b6,
         },
       ],
