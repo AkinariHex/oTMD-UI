cc9e683040699e31455dd82d59896abab1928aa6
AkinariHex <akinari@outlook.it>
2022-02-18 17:35:33 +0100

    Update app.js

--- src/pages/api/tournaments/app.js
+++ src/pages/api/tournaments/app.js
@@ -2,50 +2,50 @@ import Airtable from "airtable";
 import Cors from "cors";
 
 const cors = Cors({
-    methods: ['GET'],
-})
+  methods: ["GET"],
+});
 
 function runMiddleware(req, res, fn) {
-    return new Promise((resolve, reject) => {
-      fn(req, res, (result) => {
-        if (result instanceof Error) {
-          return reject(result)
-        }
-  
-        return resolve(result)
-      })
-    })
-  }
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
 
 export default async function handler(req, res) {
-    await runMiddleware(req, res, cors)
-    const base = new Airtable({apiKey: process.env.AIRTABLE_APIKEY}).base(process.env.AIRTABLE_BASE)
+  await runMiddleware(req, res, cors);
+  const base = new Airtable({ apiKey: process.env.AIRTABLE_APIKEY }).base(
+    process.env.AIRTABLE_BASE
+  );
 
-    if (req.method === 'GET') {
-
-        if(req.query.t){
-            base('Tournaments').select({
-                filterByFormula: `IF({Acronym} = '${req.query.t}' , TRUE())`,
-                view: "Grid view"
-            }).eachPage(function page(records, fetchNextPage) {
-
-                if(records[0]) {
-                  let tournamentName = records[0].fields.Name
-                  let multipliers = JSON.parse(records[0].fields.Multipliers)
-                  return res.status(200).json({ tournamentName, multipliers})
-                } else {
-                  return res.status(200).json({ error: "No tournament found"});
-                }
-                
-            
-            
-            }, function done(err) {
-                if (err) { 
-                  console.error(err);
-                  return;
-                }
-            });
-        }
-        
+  if (req.method === "GET") {
+    if (req.query.t) {
+      base("Tournaments")
+        .select({
+          filterByFormula: `IF({Acronym} = '${req.query.t}' , TRUE())`,
+          view: "Grid view",
+        })
+        .eachPage(
+          function page(records, fetchNextPage) {
+            if (records[0]) {
+              let tournamentName = records[0].fields.Name;
+              let multipliers = JSON.parse(records[0].fields.Multipliers);
+              return res.status(200).json({ tournamentName, multipliers });
+            } else {
+              return res.status(200).json({ error: "No tournament found" });
+            }
+          },
+          function done(err) {
+            if (err) {
+              return res.status(404).json({ error: "No tournament found" });
+            }
+          }
+        );
     }
   }
+}
