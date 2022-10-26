8b9f6cbce0a39b02b87de0455c747f3ebbe4ec9d
AkinariHex <akinari@outlook.it>
2022-10-26 17:28:43 +0200

    Fix await

--- src/pages/api/match/index.js
+++ src/pages/api/match/index.js
@@ -20,115 +20,6 @@ function runMiddleware(req, res, fn) {
 export default async function handler(req, res) {
   await runMiddleware(req, res, cors);
 
-  const match = async (body, channel) => {
-    var results = "";
-    var Data = "";
-
-    /* If match is 1vs1 or teamVS */
-    if (body.stage !== "Qualifiers") {
-      results =
-        body.scores.winner === 1
-          ? `:first_place: :red_circle: **${
-              body.matchType === "1vs1"
-                ? `${body.players[0].username} | ${body.scores.player1}`
-                : `${body.teams[0]} | ${body.scores.team1}`
-            }** - ${
-              body.matchType === "1vs1"
-                ? `${body.scores.player2} | ${body.players[1].username}`
-                : `${body.scores.team2} | ${body.teams[1]}`
-            } :blue_circle:`
-          : `:red_circle: ${
-              body.matchType === "1vs1"
-                ? `${body.players[0].username} | ${body.scores.player1}`
-                : `${body.teams[0]} | ${body.scores.team1}`
-            } - **${
-              body.matchType === "1vs1"
-                ? `${body.scores.player2} | ${body.players[1].username}`
-                : `${body.scores.team2} | ${body.teams[1]}`
-            }** :blue_circle: :first_place:`;
-
-      Data = {
-        embeds: [
-          {
-            author: {
-              name: `${body.tournament.acronym} ${
-                body.tournament.name !== "" ? `- ${body.tournament.name}` : ""
-              }`,
-              url: `https://osu.ppy.sh/community/matches/${body.matchID}`,
-              icon_url: `https://akinariosu.s-ul.eu/f72xTlsv`,
-            },
-            /* title: `(${
-            body.matchType === "1vs1" ? body.players[0].username : body.teams[0]
-          }) vs (${
-            body.matchType === "1vs1" ? body.players[1].username : body.teams[1]
-          })`, */
-            url: `https://osu.ppy.sh/community/matches/${body.matchID}`,
-            description: `${results}\n\n${body.matchType} - ${body.stage} - BO${body.bestOF} - ${body.warmups} warmups - [MP Link](https://osu.ppy.sh/community/matches/${body.matchID})`,
-            color: body.scores.winner === 1 ? 0xff4c4c : 0x4876b6,
-            footer: {
-              text: getTimeSpent(body.matchStart),
-            },
-          },
-        ],
-      };
-    }
-
-    /* If match is Qualifiers */
-    if (body.stage === "Qualifiers") {
-      var mods = {
-        NM: "<:nomod:868095234217750558>",
-        NF: "<:nofail:868095234230353960>",
-        EZ: "<:easy:868095234108710922>",
-        HD: "<:hidden:868095234150658079>",
-        HR: "<:hardrock:868095234129690664>",
-        DT: "<:doubletime:868095234079350844>",
-        HT: "<:halftime:943890234330972160>",
-        FL: "<:flashlight:868095233932533781>",
-      };
-
-      await body.scores.list.forEach((object) => {
-        let modstring = "";
-        object.mods.forEach((mod) => {
-          modstring += mods[mod];
-        });
-        results += `${modstring} ${object.score}\n`;
-      });
-
-      Data = {
-        embeds: [
-          {
-            author: {
-              name: `${body.tournament.acronym} ${
-                body.tournament.name !== "" ? `- ${body.tournament.name}` : ""
-              }`,
-              url: `https://osu.ppy.sh/community/matches/${body.matchID}`,
-              icon_url: `https://akinariosu.s-ul.eu/f72xTlsv`,
-            },
-            title: `${body.player.username}'s Qualifier`,
-            url: `https://osu.ppy.sh/community/matches/${body.matchID}`,
-            description: `${results}\n**Average: ${body.scores.average}** - Played Maps: ${body.scores.list.length} - Total maps: ${body.totalMaps} - [MP Link](https://osu.ppy.sh/community/matches/${body.matchID})`,
-            color: 0x4876b6,
-            thumbnail: {
-              url: `http://s.ppy.sh/a/${body.me}`,
-            },
-            footer: {
-              text: getTimeSpent(body.matchStart),
-            },
-          },
-        ],
-      };
-    }
-
-    await fetch(channel.WebhookURL, {
-      method: "post",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(Data),
-    });
-    return;
-  };
-
   if (req.method === "POST") {
     const body = JSON.parse(req.body);
 
@@ -177,8 +68,112 @@ export default async function handler(req, res) {
     let channels = JSON.parse(data[0].discordChannelsMatch);
 
     if (sendMatches === true && channels.length > 0) {
-      channels.forEach(async (channel) => {
-        await match(body, channel);
+      var results = "";
+      var Data = "";
+
+      /* If match is 1vs1 or teamVS */
+      if (body.stage !== "Qualifiers") {
+        results =
+          (await body.scores.winner) === 1
+            ? `:first_place: :red_circle: **${
+                body.matchType === "1vs1"
+                  ? `${body.players[0].username} | ${body.scores.player1}`
+                  : `${body.teams[0]} | ${body.scores.team1}`
+              }** - ${
+                body.matchType === "1vs1"
+                  ? `${body.scores.player2} | ${body.players[1].username}`
+                  : `${body.scores.team2} | ${body.teams[1]}`
+              } :blue_circle:`
+            : `:red_circle: ${
+                body.matchType === "1vs1"
+                  ? `${body.players[0].username} | ${body.scores.player1}`
+                  : `${body.teams[0]} | ${body.scores.team1}`
+              } - **${
+                body.matchType === "1vs1"
+                  ? `${body.scores.player2} | ${body.players[1].username}`
+                  : `${body.scores.team2} | ${body.teams[1]}`
+              }** :blue_circle: :first_place:`;
+
+        Data = {
+          embeds: [
+            {
+              author: {
+                name: `${body.tournament.acronym} ${
+                  body.tournament.name !== "" ? `- ${body.tournament.name}` : ""
+                }`,
+                url: `https://osu.ppy.sh/community/matches/${body.matchID}`,
+                icon_url: `https://akinariosu.s-ul.eu/f72xTlsv`,
+              },
+              /* title: `(${
+              body.matchType === "1vs1" ? body.players[0].username : body.teams[0]
+            }) vs (${
+              body.matchType === "1vs1" ? body.players[1].username : body.teams[1]
+            })`, */
+              url: `https://osu.ppy.sh/community/matches/${body.matchID}`,
+              description: `${results}\n\n${body.matchType} - ${body.stage} - BO${body.bestOF} - ${body.warmups} warmups - [MP Link](https://osu.ppy.sh/community/matches/${body.matchID})`,
+              color: body.scores.winner === 1 ? 0xff4c4c : 0x4876b6,
+              footer: {
+                text: await getTimeSpent(body.matchStart),
+              },
+            },
+          ],
+        };
+      }
+
+      /* If match is Qualifiers */
+      if (body.stage === "Qualifiers") {
+        var mods = {
+          NM: "<:nomod:868095234217750558>",
+          NF: "<:nofail:868095234230353960>",
+          EZ: "<:easy:868095234108710922>",
+          HD: "<:hidden:868095234150658079>",
+          HR: "<:hardrock:868095234129690664>",
+          DT: "<:doubletime:868095234079350844>",
+          HT: "<:halftime:943890234330972160>",
+          FL: "<:flashlight:868095233932533781>",
+        };
+
+        await body.scores.list.forEach((object) => {
+          let modstring = "";
+          object.mods.forEach((mod) => {
+            modstring += mods[mod];
+          });
+          results += `${modstring} ${object.score}\n`;
+        });
+
+        Data = {
+          embeds: [
+            {
+              author: {
+                name: `${body.tournament.acronym} ${
+                  body.tournament.name !== "" ? `- ${body.tournament.name}` : ""
+                }`,
+                url: `https://osu.ppy.sh/community/matches/${body.matchID}`,
+                icon_url: `https://akinariosu.s-ul.eu/f72xTlsv`,
+              },
+              title: `${body.player.username}'s Qualifier`,
+              url: `https://osu.ppy.sh/community/matches/${body.matchID}`,
+              description: `${results}\n**Average: ${body.scores.average}** - Played Maps: ${body.scores.list.length} - Total maps: ${body.totalMaps} - [MP Link](https://osu.ppy.sh/community/matches/${body.matchID})`,
+              color: 0x4876b6,
+              thumbnail: {
+                url: `http://s.ppy.sh/a/${body.me}`,
+              },
+              footer: {
+                text: await getTimeSpent(body.matchStart),
+              },
+            },
+          ],
+        };
+      }
+
+      await channels.forEach(async (channel) => {
+        await fetch(channel.WebhookURL, {
+          method: "post",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(Data),
+        });
       });
       return res.status(200).json({ status: "done" });
     } else {
